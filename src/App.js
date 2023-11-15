500b30f16706bcacf03af64374252a9eedc4f648
Amy Richardson <105483927+amylour@users.noreply.github.com>
2023-11-15 13:03:31 +0000

    App setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CenteredSquares from './CenteredSquares';
+
+function App() {
+  return (
+    <div className="App">
+      <CenteredSquares />
+    </div>
+  );
+}
+
+export default App;
